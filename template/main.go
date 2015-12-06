aaa020b349cad508582efbf9f8e867d18da087a9
Bill Hathaway <bill.hathaway@gmail.com>
2015-12-06 14:52:43 -0800

    add template

---
+++ template/main.go
@@ -0,0 +1,29 @@
+package main
+
+// boilerplate template used for Advent of Code
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func parse(r io.Reader) error {
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanLines)
+	for s.Scan() {
+		line := s.Text()
+		fmt.Println(line)
+	}
+	return nil
+}
+
+func main() {
+	err := parse(os.Stdin)
+	if err != nil {
+		fmt.Println("ERROR ", err)
+		os.Exit(1)
+	}
+	fmt.Println("OK")
+}
